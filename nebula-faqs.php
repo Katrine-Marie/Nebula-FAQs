2be294c5f57c3b960c62b610e805a2dffad1e1a4
Katrine-Marie <fjordscene@gmail.com>
2019-02-26 13:52:47 +0100

    Base setup

---
+++ nebula-faqs.php
@@ -0,0 +1,19 @@
+<?php
+/**********
+* Plugin Name: Nebula FAQs
+* Plugin URI: https://github.com/Katrine-Marie/Nebula-FAQs
+* Description: Add FAQ posts to an info accordion
+* Version: 1.0.0
+* Author: Katrine-Marie Burmeister
+* Author URI: https://fjordstudio.dk
+* License: GPL-2.0+
+License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+*/
+
+namespace nebula\faqs;
+
+if(!defined('ABSPATH')){
+	exit('Go away!');
+}
+
+define('nebula_faqs_DIR', plugin_dir_path(__FILE__));
